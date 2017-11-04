914b670e24107df66ebe3f483837f2d105044b2a
Perceu Bertoletti <perceubertoletti@gmail.com>
2017-11-04 10:17:45 -0200

    Tela de subscriptions

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+class homeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+    def test_get(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template_index(self):
+        self.resp = self.client.get('/')
+        self.assertTemplateUsed(self.resp, 'index.html')
---
+++ eventex/subscriptions/tests.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+# Create your tests here.
+class SubscribeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
+
+    def test_get(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
+    
+    def test_html(self):
+        self.assertContains(self.resp, '<form')
+        self.assertContains(self.resp, '<input', 6)
+        self.assertContains(self.resp, 'type="text"', 3)
+        self.assertContains(self.resp, 'type="email"')
+        self.assertContains(self.resp, 'type="submit"')
+
+    def test_crsftoken(self):
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
+
+    def test_form(self):
+        from eventex.subscriptions.forms import SubscriptionForm
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_fields(self):
+        form = form = self.resp.context['form']
+        self.assertListEqual(['name','cpf','email','phone'], list(form.fields))
+
+
